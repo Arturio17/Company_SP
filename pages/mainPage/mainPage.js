1fee661bee9a7758ecb07b62baad3d0341011be3
Artur <146008126+Arturio17@users.noreply.github.com>
2026-03-16 15:52:09 +0300

    Update mainPage.js

--- pages/mainPage/mainPage.js
+++ pages/mainPage/mainPage.js
@@ -1,6 +1,6 @@
 import Catalog from '../../entities/catalog/catalog.js';
 import About from '../../entities/about/about.js';
-import Readme from '../../entities/readme/readme.js';
+
 
 const MainPage = () => {
     return (
